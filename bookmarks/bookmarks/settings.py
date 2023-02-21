94912b182880b9fb98ce55f39e1514706227780b
student <jasurabdibayev0@gmail.com>
2023-02-21 16:43:44 +0500

    add login

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'account.apps.AccountConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'bookmarks.urls'
+
 
 TEMPLATES = [
     {
