7db87efaa24dfca2b7382b9a23a505601c45991a
student <jasurabdibayev0@gmail.com>
2023-02-22 17:10:51 +0500

    all

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -154,36 +154,36 @@ AUTHENTICATION_BACKENDS = [
     'account.authentication.EmailAuthBackend',
 ]
 
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'account.authentication.EmailAuthBackend',
-    'social_core.backends.facebook.FacebookOAuth2',
-    'social_core.backends.twitter.TwitterOAuth',
-    'social_core.backends.google.GoogleOAuth2',
-]
+# AUTHENTICATION_BACKENDS = [
+#     'django.contrib.auth.backends.ModelBackend',
+#     'account.authentication.EmailAuthBackend',
+#     'social_core.backends.facebook.FacebookOAuth2',
+#     'social_core.backends.twitter.TwitterOAuth',
+#     'social_core.backends.google.GoogleOAuth2',
+# ]
 
-SOCIAL_AUTH_FACEBOOK_KEY = '' # Facebook App ID
-SOCIAL_AUTH_FACEBOOK_SECRET = '' # Facebook App Secret
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
-
-SOCIAL_AUTH_TWITTER_KEY = '' # Twitter API Key
-SOCIAL_AUTH_TWITTER_SECRET = '' # Twitter API Secret
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '' # Google Client ID
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '' # Google Client Secret
-
-SOCIAL_AUTH_PIPELINE = [
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',
-    'account.authentication.create_profile',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-]
+# SOCIAL_AUTH_FACEBOOK_KEY = '' # Facebook App ID
+# SOCIAL_AUTH_FACEBOOK_SECRET = '' # Facebook App Secret
+# SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+#
+# SOCIAL_AUTH_TWITTER_KEY = '' # Twitter API Key
+# SOCIAL_AUTH_TWITTER_SECRET = '' # Twitter API Secret
+#
+# SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '' # Google Client ID
+# SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '' # Google Client Secret
+#
+# SOCIAL_AUTH_PIPELINE = [
+#     'social_core.pipeline.social_auth.social_details',
+#     'social_core.pipeline.social_auth.social_uid',
+#     'social_core.pipeline.social_auth.auth_allowed',
+#     'social_core.pipeline.social_auth.social_user',
+#     'social_core.pipeline.user.get_username',
+#     'social_core.pipeline.user.create_user',
+#     'account.authentication.create_profile',
+#     'social_core.pipeline.social_auth.associate_user',
+#     'social_core.pipeline.social_auth.load_extra_data',
+#     'social_core.pipeline.user.user_details',
+# ]
 
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail',
